48bc492831e32c6fda2b9eaae45ba16db695fc9b
Fisnik <71096106+Doommafia@users.noreply.github.com>
2024-08-28 19:54:41 +0200

    Create autoclick.py

---
+++ autoclick.py
@@ -0,0 +1,42 @@
+import pyautogui
+import time
+import random
+import keyboard
+
+min_interval = 0.3
+max_interval = 0.7
+
+interval_count = 0
+interval_sum = 0 
+clicking = False  
+
+def autoclick():
+    global clicking
+    clicking = not clicking
+    if clicking:
+        print("Autoclicker turned ON.")
+    else:
+        print("Autoclicker turned OFF.")
+
+keyboard.add_hotkey('j', autoclick)
+
+try:
+    while True:
+        if clicking:
+            pyautogui.click()
+
+            interval = random.uniform(min_interval, max_interval)
+            interval_count += 1
+            interval_sum += interval
+
+            print(f"click - {interval:.3f}s")
+            time.sleep(interval)
+        else:
+            time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("Autoclicker stopped.")
+    if interval_count > 0:
+        print(f"Average interval was {(interval_sum / interval_count):.3f}s!")
+    else:
+        print("No clicks were made.")
